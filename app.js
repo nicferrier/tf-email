9130ff86a244b9c976c9e26b9f4903f201096200
nicferrier <nic@ferrier.me.uk>
2017-06-04 16:10:04 +0200

    collect all the sent stuff together

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var ImapClient = require("emailjs-imap-client");
 var client = new ImapClient("localhost", 143, {
     auth: {
@@ -6,28 +8,75 @@ var client = new ImapClient("localhost", 143, {
     }
 });
 
+setTimeout(() => {
+    client.close();
+}, 2 * 60 * 1000);
+
+
 client.onerror = function (err) {
     console.log("some imap error", err);
 }
 
 
 client.connect().then(() => {
+    client.listMailboxes()
+	.then((mailboxes) => {
+	    console.log("mailboxes", mailboxes);
+	});
     client.selectMailbox("INBOX")
 	.then((mailbox) => {
 	    console.log("mailbox", mailbox);
 
-	    client.listMessages("INBOX", "1:5", ["uid", "flags", "body", "envelope"])
+	    client.listMessages("INBOX", "1:5000", ["uid", "flags", "body", "envelope"])
 		.then((messages) => {
-		    console.log(JSON.stringify(messages,null,2));
+		    var to = {};
+		    messages.forEach((v, i, arr) => {
+			try {
+			    var uid = v.uid;
+			    var env = v.envelope;
+			    var toEmail = env.to[0].address;
+			    if (to[toEmail] == null) {
+				to[toEmail] = [v];
+			    }
+			    else {
+				to[toEmail].push(v);
+			    }
+			}
+			catch (e) {
+			    console.log("error", e);
+			}
+		    });
+		    presentEmails(to);
 		});
 	});
     
 });
 
 
-
-setTimeout(() => {
-    client.close();
-}, 2 * 60 * 1000);
+function presentEmails (list) {
+    var struct = {};
+    Object.keys(list).forEach((email) => {
+	var messages = list[email];
+	messages.forEach((v, i, arr) => {
+	    if (struct[email] == null) {
+		struct[email] = {};
+	    }
+	    
+	    var uid = v.uid;
+	    var env = v.envelope;
+	    var fromEmail = env.from[0].address;
+	    var replyEmail = env["reply-to"][0].address;
+	    var senderEmail = env.sender[0].address;
+	    if (struct[email][fromEmail] != null) {
+		struct[email][fromEmail].push(v);
+	    }
+	    else {
+		struct[email][fromEmail] = [v];
+	    }
+	});
+    });
+    console.log(JSON.stringify(struct, null, 2));
+    
+}
 
 // app.js ends here
